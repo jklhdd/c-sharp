3d420e0d7d602c8597a0ff377bdbdbef62a6f1bd
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-01-08 15:15:11 +0700

    nopbaitap

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(count);
+            t.Start();
+            
+
+        }
+        public static void count()
+        {
+            for(int i = 600;i > 0; i--)
+            {
+                Console.WriteLine(i / 60 +":"+ i % 60);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
