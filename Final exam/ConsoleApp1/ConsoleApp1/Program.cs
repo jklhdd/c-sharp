5ebaca22b97c34e0f2a277352557ddee8d5bbb4c
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-02 16:00:34 +0700

    nopbai

---
+++ Final exam/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone testPhone = new Phone("Long","co dinh",80000);
+            MobilePhone testMobilePhone = new MobilePhone();
+            testPhone.display();
+            testMobilePhone.display();
+        }
+    }
+}
