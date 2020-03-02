0027ed86c478638d4f1c0a64378facd1125ed952
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-02 16:18:50 +0700

    nopbai

--- Final exam/ConsoleApp1/ConsoleApp1/Program.cs
+++ Final exam/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,14 +2,26 @@
 
 namespace ConsoleApp1
 {
+    public delegate void PrimeNumberFinder();
     class Program
     {
         static void Main(string[] args)
         {
+            
             Phone testPhone = new Phone("Long","co dinh",80000);
             MobilePhone testMobilePhone = new MobilePhone();
             testPhone.display();
             testMobilePhone.display();
         }
+        public event PrimeNumberFinder OnPrimeNumber;
+
+        public void PrimeNumberFinder()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
+        }
     }
+    
 }
